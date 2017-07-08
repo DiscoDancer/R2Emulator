a08f2be2e3cb037ee50eb667a4ba67d4d3b748dd
tsepes.vlad <tsepes.vlad@hotmail.com>
2017-07-08 15:48:50 +0300

    added first drop items system
    also a lot of refactoring

---
+++ goblin/items.js
@@ -0,0 +1,33 @@
+var _items = [
+                {
+                    id: 1,
+                    description: 'Блестящий шлем, изготовленный из металла.',
+                    name: 'Шлем гоблина',
+                    rate: 5,
+                    imageUrl: 'Helmet.jpg'
+                }, {
+                    id: 2,
+                    description: 'Доспех гоблина.',
+                    name: 'Доспех гоблина',
+                    rate: 10,
+                    imageUrl: 'Armor.jpg'
+                }, {
+                    id: 3,
+                    description: 'Сапоги, изготовленные гоблином.',
+                    name: 'Сапоги гоблина',
+                    rate: 5,
+                    imageUrl: 'Boots.jpg'
+                }, {
+                    id: 4,
+                    description: 'Маленький и легкий щит, который может использовать кто угодно.',
+                    name: 'Круглый щит',
+                    rate: 5,
+                    imageUrl: 'Shield.jpg'
+                }, {
+                    id: 5,
+                    description: 'Часть гоблинской кольчуги.',
+                    name: 'Часть гоблинской кольчуги',
+                    rate: 5,
+                    imageUrl: 'PartOfMail.jpg'
+                }
+            ];
