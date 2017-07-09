69669b7e77ecfebe78dcf82b1137f2167d5ccc07
tsepes.vlad <tsepes.vlad@hotmail.com>
2017-07-09 15:06:15 +0300

    started inventory system

--- goblin/items.js
+++ goblin/items.js
@@ -1,33 +1,61 @@
+var _item_types = [
+    {
+        id: 1,
+        name: 'helmet'
+    },
+    {
+        id: 2,
+        name: 'armor'
+    },
+    {
+        id: 3,
+        name: 'boots'
+    },
+    {
+        id: 4,
+        name: 'shield'
+    },
+    {
+        id: 5,
+        name: 'ingredient'
+    },
+];
+
 var _items = [
                 {
                     id: 1,
                     description: 'Блестящий шлем, изготовленный из металла.',
                     name: 'Шлем гоблина',
                     rate: 5,
-                    imageUrl: 'Helmet.jpg'
+                    imageUrl: 'Helmet.jpg',
+                    typeId: 1
                 }, {
                     id: 2,
                     description: 'Доспех гоблина.',
                     name: 'Доспех гоблина',
                     rate: 10,
-                    imageUrl: 'Armor.jpg'
+                    imageUrl: 'Armor.jpg',
+                    typeId: 2
                 }, {
                     id: 3,
                     description: 'Сапоги, изготовленные гоблином.',
                     name: 'Сапоги гоблина',
                     rate: 5,
-                    imageUrl: 'Boots.jpg'
+                    imageUrl: 'Boots.jpg',
+                    typeId: 3
                 }, {
                     id: 4,
                     description: 'Маленький и легкий щит, который может использовать кто угодно.',
                     name: 'Круглый щит',
                     rate: 5,
-                    imageUrl: 'Shield.jpg'
+                    imageUrl: 'Shield.jpg',
+                    typeId: 4
                 }, {
                     id: 5,
                     description: 'Часть гоблинской кольчуги.',
                     name: 'Часть гоблинской кольчуги',
                     rate: 5,
-                    imageUrl: 'PartOfMail.jpg'
+                    imageUrl: 'PartOfMail.jpg',
+                    typeId: 5
                 }
             ];
