6971fdf31aac40b666dd456b728e56fb86184668
DusciCat <duscicat@gmail.com>
2017-07-21 12:59:54 +0300

    Drop of Gremlin
    Added a drop of gremlin and a description of it

--- goblin/helper.js
+++ goblin/helper.js
@@ -1,3 +1,7 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
+function RandomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
--- goblin/items.js
+++ goblin/items.js
@@ -20,6 +20,48 @@ var _item_types = [
         name: 'ingredient'
     },
 ];
+var _item_type = [
+    {
+        id: 1,
+        name: 'hat_gr'
+    },
+    {
+        id: 2,
+        name: 'gloves_gr'
+    },
+    {
+        id: 3,
+        name: 'sword_gr'
+    },
+    {
+        id: 4,
+        name: 'helmet_gr'
+    },
+    {
+        id: 5,
+        name: 'boots_gr'
+    },
+    {
+        id: 6,
+        name: 'shield_gr'
+    },
+    {
+        id: 7,
+        name: 'sword1_gr'
+    },
+    {
+        id: 8,
+        name: 'axe_gr'
+    },
+    {
+        id: 9,
+        name: 'book_gr'
+    },
+    {
+        id: 10,
+        name: 'armor_gr'
+    },
+]
 
 var _items = [
                 {
@@ -57,5 +99,79 @@ var _items = [
                     rate: 5,
                     imageUrl: 'PartOfMail.jpg',
                     typeId: 5
-                }
+                },
+            ];
+            var _item = [
+                {
+                    id: 1,
+                    description: 'Шляпа, изготовленная из простой кожи.',
+                    name: 'Кожаная шляпа',
+                    rate: 5,
+                    imageUrl: 'Hat_gr.jpg',
+                    typeId: 5
+                }, {
+                    id: 2,
+                    description: 'Перчатки, изготовленные из простой кожи.',
+                    name: 'Легкие кожаные перчатки',
+                    rate: 5,
+                    imageUrl: 'Gloves_gr.jpg',
+                    typeId: 7
+                }, {
+                    id: 3,
+                    description: 'Длинный и узкий клинок.',
+                    name: 'Шпага',
+                    rate: 5,
+                    imageUrl: 'Sword1_gr.jpg',
+                    typeId: 8
+                }, {
+                    id: 4,
+                    description: 'Шлем, изготовленный из кожи.',
+                    name: 'Кожаный шлем',
+                    rate: 5,
+                    imageUrl: 'Helmet_gr.jpg',
+                    typeId: 9
+                }, {
+                    id: 5,
+                    description: 'Сапоги из обычной кожи.',
+                    name: 'Легкие кожаные сапоги',
+                    rate: 5,
+                    imageUrl: 'Boots_gr.jpg',
+                    typeId: 10
+                }, {
+                    id: 6,
+                    description: 'Прочный щит, изготовленный из дерева.',
+                    name: 'Деревянный щит',
+                    rate: 5,
+                    imageUrl: 'Shield_gr.jpg',
+                    typeId: 11
+                }, {
+                    id: 7,
+                    description: 'Хороший охотничий кинжал.',
+                    name: 'Охотничий кинжал',
+                    rate: 5,
+                    imageUrl: 'Sword_gr.jpg',
+                    typeId: 12
+                }, {
+                    id: 8,
+                    description: 'Топор, изготовленный из камня.',
+                    name: 'Каменный топор',
+                    rate: 5,
+                    imageUrl: 'Axe_gr.jpg',
+                    typeId: 13
+                }, {
+                    id: 9,
+                    description: 'Заклинание, создающее еду.',
+                    name: 'Создание еды',
+                    rate: 5,
+                    imageUrl: 'Book_gr.jpg',
+                    typeId: 14
+                }, {
+                    id: 10,
+                    description: 'Доспех из толстой кожи.',
+                    name: 'Кожаный доспех',
+                    rate: 10,
+                    imageUrl: 'Armor_gr.jpg',
+                    typeId: 15
+                },
+
             ];
