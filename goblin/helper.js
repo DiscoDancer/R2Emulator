3e8a77252b72cfcf191fcc56dae5ed1eb57a64ae
tsepes.vlad <tsepes.vlad@hotmail.com>
2017-07-23 19:24:55 +0300

    - refactoring + started to implement equipment

--- goblin/helper.js
+++ goblin/helper.js
@@ -1,7 +1,4 @@
 function getRandomNumber(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
-function RandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-}
 
--- goblin/items.js
+++ goblin/items.js
@@ -20,154 +20,124 @@ var _item_types = [
         name: 'ingredient'
     },
     {
-        id: 1,
-        name: 'hat_gr'
-    },
-    {
-        id: 2,
-        name: 'gloves_gr'
-    },
-    {
-        id: 3,
-        name: 'sword_gr'
-    },
-    {
-        id: 4,
-        name: 'helmet_gr'
-    },
-    {
-        id: 5,
-        name: 'boots_gr'
-    },
-    {
         id: 6,
-        name: 'shield_gr'
+        name: 'gloves'
     },
     {
         id: 7,
-        name: 'sword1_gr'
-    },
-    {
-        id: 8,
-        name: 'axe_gr'
-    },
-    {
-        id: 9,
-        name: 'book_gr'
-    },
-    {
-        id: 10,
-        name: 'armor_gr'
-    },
+        name: 'sword'
+    }
 ];
 
-var _items = [
-                {
-                    id: 1,
-                    description: 'Блестящий шлем, изготовленный из металла.',
-                    name: 'Шлем гоблина',
-                    rate: 5,
-                    imageUrl: 'Helmet.jpg',
-                    typeId: 1
-                }, {
-                    id: 2,
-                    description: 'Доспех гоблина.',
-                    name: 'Доспех гоблина',
-                    rate: 10,
-                    imageUrl: 'Armor.jpg',
-                    typeId: 2
-                }, {
-                    id: 3,
-                    description: 'Сапоги, изготовленные гоблином.',
-                    name: 'Сапоги гоблина',
-                    rate: 5,
-                    imageUrl: 'Boots.jpg',
-                    typeId: 3
-                }, {
-                    id: 4,
-                    description: 'Маленький и легкий щит, который может использовать кто угодно.',
-                    name: 'Круглый щит',
-                    rate: 5,
-                    imageUrl: 'Shield.jpg',
-                    typeId: 4
-                }, {
-                    id: 5,
-                    description: 'Часть гоблинской кольчуги.',
-                    name: 'Часть гоблинской кольчуги',
-                    rate: 5,
-                    imageUrl: 'PartOfMail.jpg',
-                    typeId: 5
-                },  {
-                    id: 6,
-                    description: 'Шляпа, изготовленная из простой кожи.',
-                    name: 'Кожаная шляпа',
-                    rate: 5,
-                    imageUrl: 'Hat_gr.jpg',
-                    typeId: 6
-                }, {
-                    id: 7,
-                    description: 'Перчатки, изготовленные из простой кожи.',
-                    name: 'Легкие кожаные перчатки',
-                    rate: 5,
-                    imageUrl: 'Gloves_gr.jpg',
-                    typeId: 7
-                }, {
-                    id: 8,
-                    description: 'Длинный и узкий клинок.',
-                    name: 'Шпага',
-                    rate: 5,
-                    imageUrl: 'Sword1_gr.jpg',
-                    typeId: 8
-                }, {
-                    id: 9,
-                    description: 'Шлем, изготовленный из кожи.',
-                    name: 'Кожаный шлем',
-                    rate: 5,
-                    imageUrl: 'Helmet_gr.jpg',
-                    typeId: 9
-                }, {
-                    id: 10,
-                    description: 'Сапоги из обычной кожи.',
-                    name: 'Легкие кожаные сапоги',
-                    rate: 5,
-                    imageUrl: 'Boots_gr.jpg',
-                    typeId: 10
-                }, {
-                    id: 11,
-                    description: 'Прочный щит, изготовленный из дерева.',
-                    name: 'Деревянный щит',
-                    rate: 5,
-                    imageUrl: 'Shield_gr.jpg',
-                    typeId: 11
-                }, {
-                    id: 12,
-                    description: 'Хороший охотничий кинжал.',
-                    name: 'Охотничий кинжал',
-                    rate: 5,
-                    imageUrl: 'Sword_gr.jpg',
-                    typeId: 12
-                }, {
-                    id: 13,
-                    description: 'Топор, изготовленный из камня.',
-                    name: 'Каменный топор',
-                    rate: 5,
-                    imageUrl: 'Axe_gr.jpg',
-                    typeId: 13
-                }, {
-                    id: 14,
-                    description: 'Заклинание, создающее еду.',
-                    name: 'Создание еды',
-                    rate: 5,
-                    imageUrl: 'Book_gr.jpg',
-                    typeId: 14
-                }, {
-                    id: 15,
-                    description: 'Доспех из толстой кожи.',
-                    name: 'Кожаный доспех',
-                    rate: 10,
-                    imageUrl: 'Armor_gr.jpg',
-                    typeId: 15
-                },
+var _items_goblin = [
+    {
+        id: 1,
+        description: 'Блестящий шлем, изготовленный из металла.',
+        name: 'Шлем гоблина',
+        rate: 5,
+        imageUrl: 'Helmet.jpg',
+        typeId: 1
+    }, {
+        id: 2,
+        description: 'Доспех гоблина.',
+        name: 'Доспех гоблина',
+        rate: 10,
+        imageUrl: 'Armor.jpg',
+        typeId: 2
+    }, {
+        id: 3,
+        description: 'Сапоги, изготовленные гоблином.',
+        name: 'Сапоги гоблина',
+        rate: 5,
+        imageUrl: 'Boots.jpg',
+        typeId: 3
+    }, {
+        id: 4,
+        description: 'Маленький и легкий щит, который может использовать кто угодно.',
+        name: 'Круглый щит',
+        rate: 5,
+        imageUrl: 'Shield.jpg',
+        typeId: 4
+    }, {
+        id: 5,
+        description: 'Часть гоблинской кольчуги.',
+        name: 'Часть гоблинской кольчуги',
+        rate: 5,
+        imageUrl: 'PartOfMail.jpg',
+        typeId: 5
+    }
+];
 
-            ];
-            
+var _items_gremlin = [
+    {
+        id: 6,
+        description: 'Шляпа, изготовленная из простой кожи.',
+        name: 'Кожаная шляпа',
+        rate: 5,
+        imageUrl: 'Hat_gr.jpg',
+        typeId: 1
+    }, {
+        id: 7,
+        description: 'Перчатки, изготовленные из простой кожи.',
+        name: 'Легкие кожаные перчатки',
+        rate: 5,
+        imageUrl: 'Gloves_gr.jpg',
+        typeId: 6
+    }, {
+        id: 8,
+        description: 'Длинный и узкий клинок.',
+        name: 'Шпага',
+        rate: 5,
+        imageUrl: 'Sword1_gr.jpg',
+        typeId: 7
+    }, {
+        id: 9,
+        description: 'Шлем, изготовленный из кожи.',
+        name: 'Кожаный шлем',
+        rate: 5,
+        imageUrl: 'Helmet_gr.jpg',
+        typeId: 1
+    }, {
+        id: 10,
+        description: 'Сапоги из обычной кожи.',
+        name: 'Легкие кожаные сапоги',
+        rate: 5,
+        imageUrl: 'Boots_gr.jpg',
+        typeId: 3
+    }, {
+        id: 11,
+        description: 'Прочный щит, изготовленный из дерева.',
+        name: 'Деревянный щит',
+        rate: 5,
+        imageUrl: 'Shield_gr.jpg',
+        typeId: 4
+    }, {
+        id: 12,
+        description: 'Хороший охотничий кинжал.',
+        name: 'Охотничий кинжал',
+        rate: 5,
+        imageUrl: 'Sword_gr.jpg',
+        typeId: 7
+    }, {
+        id: 13,
+        description: 'Топор, изготовленный из камня.',
+        name: 'Каменный топор',
+        rate: 5,
+        imageUrl: 'Axe_gr.jpg',
+        typeId: 7
+    }, {
+        id: 14,
+        description: 'Заклинание, создающее еду.',
+        name: 'Создание еды',
+        rate: 5,
+        imageUrl: 'Book_gr.jpg',
+        typeId: 5
+    }, {
+        id: 15,
+        description: 'Доспех из толстой кожи.',
+        name: 'Кожаный доспех',
+        rate: 10,
+        imageUrl: 'Armor_gr.jpg',
+        typeId: 2
+    }
+];
